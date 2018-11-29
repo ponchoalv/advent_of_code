534ad952bcd0905d7357c6ee447ccbc04b787dfd
ponchoalv <Agoo8Agh>
2018-11-29 16:20:05 -0300

    se agrega struct y se implementa el iterador

--- src/day_3_2017.rs
+++ src/day_3_2017.rs
@@ -5,6 +5,87 @@ enum Direction {
     Left { pair: (i32, i32) },
 }
 
+enum DirectionIter {
+    Up(bool),
+    Down,
+    Right,
+    Left,
+}
+
+struct SpiralStruct {
+    pair: (i32, i32),
+    direction: DirectionIter,
+    count: i32,
+}
+
+impl SpiralStruct {
+    fn new(pair: (i32, i32), initial_direction: DirectionIter) -> SpiralStruct {
+        SpiralStruct {
+            pair,
+            direction: initial_direction,
+            count: 2,
+        }
+    }
+
+    fn update_direction(&mut self) {
+        self.direction = match &self.direction {
+            DirectionIter::Up(true) => DirectionIter::Up(false),
+            DirectionIter::Up(false) => DirectionIter::Left,
+            DirectionIter::Down => DirectionIter::Right,
+            DirectionIter::Left => DirectionIter::Down,
+            DirectionIter::Right => DirectionIter::Up(true),
+        }
+    }
+
+    fn update_position(&mut self) {
+        self.pair = match &self.direction {
+            DirectionIter::Up(true) => (self.pair.0 + 1, self.pair.1),
+            DirectionIter::Up(false) => (self.pair.0, self.pair.1 + 1),
+            DirectionIter::Down => (self.pair.0, self.pair.1 - 1),
+            DirectionIter::Left => (self.pair.0 - 1, self.pair.1),
+            DirectionIter::Right => (self.pair.0 + 1, self.pair.1),
+        }
+    }
+}
+
+fn get_level_for_value(number: i32) -> i32 {
+    if number == 1 {
+        1
+    } else {
+        (((number as f32).sqrt() + 1.0) / 2.0).ceil() as i32 - 1
+    }
+}
+
+impl Iterator for SpiralStruct {
+    type Item = (i32, i32);
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        let count = self.count;
+        self.count += 1;
+        let level = get_level_for_value(count) * 2;
+        let change_direction = count % level == 1;
+
+        match (&self.direction, change_direction)  {
+            (DirectionIter::Up(true), _) |  (_, true) => {
+                self.update_position();
+                self.update_direction();
+            }
+            (_, false) => self.update_position(),
+        }
+
+        Some(self.pair)
+    }
+}
+
+pub fn get_distance_with_spiral_struct(number: i32) -> i32 {
+    let spiral_struct = SpiralStruct::new((0, 0),
+                                          DirectionIter::Up(true));
+
+    let value = spiral_struct.take((number - 1) as usize).last().unwrap();
+
+    (value.0.abs() + value.1.abs())
+}
+
 fn get_new_direction(direction: Direction, change_dir: bool) -> Direction {
     match (direction, change_dir) {
         (Direction::Up { pair, .. }, true) => Direction::Left { pair },
@@ -18,9 +99,6 @@ fn get_new_direction(direction: Direction, change_dir: bool) -> Direction {
     }
 }
 
-fn get_level_for_value(number: i32) -> i32 {
-    (((number as f32).sqrt() + 1.0) / 2.0).ceil() as i32 - 1
-}
 
 fn get_spiral_iter(max: i32) -> impl Iterator<Item=(i32, i32)> {
     let mut direction = Direction::Up { is_first: true, pair: (0, 0) };
@@ -86,6 +164,34 @@ pub fn day_3_1_2017(num: i32) -> i32 {
         let posicion = get_number_position_on_level(num, level);
         ((posicion - level).abs() + level)
     }
+}
+
+
+#[test]
+fn test_spiral_struct() {
+//    let input = 1;
+//    let distance = get_distance_for_number(input);
+//    assert_eq!(0, distance);
+
+    let input = 2;
+    let distance = get_distance_with_spiral_struct(input);
+    assert_eq!(1, distance);
+
+    let input = 3;
+    let distance = get_distance_with_spiral_struct(input);
+    assert_eq!(2, distance);
+
+    let input = 12;
+    let distance = get_distance_with_spiral_struct(input);
+    assert_eq!(3, distance);
+
+    let input = 1024;
+    let distance = get_distance_with_spiral_struct(input);
+    assert_eq!(31, distance);
+
+    let input = 361527;
+    let distance = get_distance_with_spiral_struct(input);
+    assert_eq!(326, distance);
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod day_3_2017;
 
 use day_1_2017::day_1_2017;
 use day_2_2017::{day_2_2017,day_2_2_2017};
-use day_3_2017::{day_3_1_2017,get_distance_for_number};
+use day_3_2017::{day_3_1_2017,get_distance_for_number,get_distance_with_spiral_struct};
 
 
 fn main() {
@@ -41,6 +41,8 @@ fn main() {
 
     println!("    * Day 3 - Part 1 ==> {}", day_3_1_2017(361_527));
 
-    println!("    * Day 3 - Part 1 (with spiral) ==> {}", get_distance_for_number(361_527));
+    println!("    * Day 3 - Part 1 (with spiral iter) ==> {}", get_distance_for_number(361_527));
+
+    println!("    * Day 3 - Part 1 (with spiral struct iter impl) ==> {}", get_distance_with_spiral_struct(361_527));
 
 }
