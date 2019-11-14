22acb7e65c66676f0fd4ba9878f34b3d90cc1c09
Alfonso Alvarez <ponchoalv@gmail.com>
2019-11-14 16:43:34 -0300

    Added day 4 for year 2018

--- src/year_2018/day_3_2018.rs
+++ src/year_2018/day_3_2018.rs
@@ -1,9 +1,9 @@
-use std::cmp::{max, min};
+use std::collections::HashMap;
 use std::str::FromStr;
 
 use crate::util::read_input_as_string;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 struct Rectangle {
     id: u32,
     left_top: (u32, u32),
@@ -12,42 +12,52 @@ struct Rectangle {
     right_bottom: (u32, u32),
 }
 
-pub fn day_3_1_2018() -> u32 {
+pub fn day_3_1_2018() -> Option<u32> {
     let day_3_input: Vec<String> = read_input_as_string("day_3_2018.txt");
-    let mut amount_overlaps = 0;
+
     let transformed_input: Vec<Rectangle> =
         day_3_input.clone().iter().map(normalize_point).collect();
-    for i in 0..10 {
-        for j in 0..day_3_input.len() {
-            if j == i {
-                continue;
-            }
 
-            let overlap_x_y = overlaps(
-                transformed_input.get(i).unwrap().clone(),
-                transformed_input.get(j).unwrap().clone(),
+    println!(
+        "this is equal {equal}, this are not {not_equal}",
+        equal = ((3, 4) == (3, 4)),
+        not_equal = (3, 4) == (4, 3)
+    );
+
+    match (
+        get_min_x(transformed_input.clone()),
+        get_max_x(transformed_input.clone()),
+        get_min_y(transformed_input.clone()),
+        get_max_y(transformed_input.clone()),
+    ) {
+        (Some(min_x), Some(max_x), Some(min_y), Some(max_y)) => {
+            println!(
+                "min_x={min_x}, max_x={max_x}, min_y={min_y}, max_y={max_y}",
+                min_x = min_x,
+                max_x = max_x,
+                min_y = min_y,
+                max_y = max_y
             );
 
+            let map_points = get_initial_map(Rectangle {
+                id: 9999,
+                left_top: (min_x, min_y),
+                right_top: (max_x, min_y),
+                left_bottom: (min_x, max_y),
+                right_bottom: (max_x, max_y),
+            });
 
-            if overlap_x_y > 0 {
-                amount_overlaps += overlap_x_y;
-                println!(
-                    "{first:?} and {second:?} inches -> {inches}",
-                    first = transformed_input.get(i).unwrap().clone(),
-                    second = transformed_input.get(j).unwrap().clone(),
-                    inches = amount_overlaps
-                );
-                break;
-            }
+            let counted_map = get_counted_points(map_points, transformed_input);
+
+            Some(
+                counted_map
+                    .iter()
+                    .filter(|((_, _), &count)| count > 1)
+                    .count() as u32,
+            )
         }
-        println!(
-            "Amount on iteration {i} -> {amount} | rectangle -> {rect:?}",
-            i = i,
-            amount = amount_overlaps,
-            rect = *transformed_input.get(i).unwrap()
-        );
+        _ => None,
     }
-    amount_overlaps
 }
 
 fn normalize_point(line: &String) -> Rectangle {
@@ -78,7 +88,7 @@ fn normalize_point(line: &String) -> Rectangle {
             .get(1)
             .unwrap(),
     )
-        .unwrap();
+    .unwrap();
 
     let origin_string = id_and_origin
         .clone()
@@ -105,41 +115,9 @@ fn vec_to_tuple(vec: Vec<&str>) -> (u32, u32) {
     )
 }
 
-fn overlaps(left_rectangle: Rectangle, right_rectangle: Rectangle) -> u32 {
-    /*
-        println!(
-            "left_rectangle.left_top.0 -> {left_rectangle1}",
-            left_rectangle1 = left_rectangle.left_top.0
-        );
-        println!(
-            "right_rectangle.right_bottom.0 -> {left_rectangle2}",
-            left_rectangle2 = right_rectangle.right_bottom.0
-        );
-        println!(
-            "right_rectangle.left_top.0 -> {left_rectangle3}",
-            left_rectangle3 = right_rectangle.left_top.0
-        );
-        println!(
-            "left_rectangle.right_bottom.0 -> {left_rectangle4}",
-            left_rectangle4 = left_rectangle.right_bottom.0
-        );
-        println!(
-            "left_rectangle.left_top.1 -> {left_rectangle5}",
-            left_rectangle5 = left_rectangle.left_top.1
-        );
-        println!(
-            "right_rectangle.right_bottom.1 -> {left_rectangle6}",
-            left_rectangle6 = right_rectangle.right_bottom.1
-        );
-        println!(
-            "right_rectangle.left_top.1 -> {left_rectangle7}",
-            left_rectangle7 = right_rectangle.left_top.1
-        );
-        println!(
-            "left_rectangle.right_bottom.1 -> {left_rectangle8}",
-            left_rectangle8 = left_rectangle.right_bottom.1
-        );
-    */
+/*
+Another implementation for checking overlaping between two rectangles.
+fn count_overlaps_inchs(left_rectangle: Rectangle, right_rectangle: Rectangle) -> u32 {
     let x_overlap = max(
         0,
         min(left_rectangle.right_top.0, right_rectangle.right_top.0) as i32
@@ -153,27 +131,139 @@ fn overlaps(left_rectangle: Rectangle, right_rectangle: Rectangle) -> u32 {
     ) as u32;
 
     x_overlap * y_overlap
+}
+*/
 
-    /*if (left_rectangle.right_top.0 < right_rectangle.left_top.0
+fn is_overlaping(left_rectangle: Rectangle, right_rectangle: Rectangle) -> bool {
+    if (left_rectangle.right_top.0 < right_rectangle.left_top.0
         || right_rectangle.right_top.0 < left_rectangle.left_top.0)
         || (right_rectangle.left_top.1 > left_rectangle.left_bottom.1
-        || left_rectangle.left_top.1 > right_rectangle.left_bottom.1)
+            || left_rectangle.left_top.1 > right_rectangle.left_bottom.1)
     {
         return false;
     }
-    true*/
+    true
 }
 
-pub fn day_3_2_2018() -> u32 {
-    32
+fn get_min_x(rectangles: Vec<Rectangle>) -> Option<u32> {
+    rectangles
+        .clone()
+        .iter()
+        .map(|rectangle| rectangle.left_top.0)
+        .min()
+}
+
+fn get_max_x(rectangles: Vec<Rectangle>) -> Option<u32> {
+    rectangles
+        .clone()
+        .iter()
+        .map(|rectangle| rectangle.right_top.0)
+        .max()
+}
+
+fn get_min_y(rectangles: Vec<Rectangle>) -> Option<u32> {
+    rectangles
+        .clone()
+        .iter()
+        .map(|rectangle| rectangle.left_top.1)
+        .min()
+}
+
+fn get_max_y(rectangles: Vec<Rectangle>) -> Option<u32> {
+    rectangles
+        .clone()
+        .iter()
+        .map(|rectangle| rectangle.left_bottom.1)
+        .max()
+}
+
+fn get_initial_map(rectangle: Rectangle) -> HashMap<(u32, u32), u8> {
+    let x_range = rectangle.left_top.0..rectangle.right_top.0;
+    let y_range = rectangle.left_top.1..rectangle.left_bottom.1;
+
+    let mut initial_map: HashMap<(u32, u32), u8> =
+        HashMap::with_capacity(x_range.len() * (y_range.len() * 2) as usize);
+
+    for x in x_range {
+        for y in y_range.clone() {
+            initial_map.insert((x, y), 0);
+        }
+    }
+
+    println!("Initial map len-> {map:?}", map = initial_map.len());
+    initial_map
+}
+
+fn get_rectangle_points(rectangle: Rectangle) -> Vec<(u32, u32)> {
+    let x_range = rectangle.left_top.0..rectangle.right_top.0;
+    let y_range = rectangle.left_top.1..rectangle.left_bottom.1;
+
+    let mut points: Vec<(u32, u32)> = Vec::with_capacity(x_range.len() * (y_range.len() * 2));
+
+    for x in x_range {
+        for y in y_range.clone() {
+            points.push((x, y));
+        }
+    }
+
+    points
+}
+
+fn get_counted_points(
+    initial_map: HashMap<(u32, u32), u8>,
+    rectangles: Vec<Rectangle>,
+) -> HashMap<(u32, u32), u8> {
+    let mut point_counter = initial_map.clone();
+
+    let rectangles_points = rectangles
+        .into_iter()
+        .flat_map(get_rectangle_points)
+        .collect::<Vec<(u32, u32)>>();
+    println!(
+        "rectangle_points len_vec={len_vec}",
+        len_vec = rectangles_points.len()
+    );
+
+    for point in rectangles_points {
+        point_counter.insert(point, point_counter[&point] + 1);
+    }
+
+    point_counter
+}
+
+pub fn day_3_2_2018() -> Option<u32> {
+    let day_3_input: Vec<String> = read_input_as_string("day_3_2018.txt");
+
+    let transformed_input: Vec<Rectangle> =
+        day_3_input.clone().iter().map(normalize_point).collect();
+
+    let mut result: Option<u32> = None;
+
+    for left in transformed_input.clone() {
+        let mut overlaps_counter = 0;
+
+        for right in transformed_input.clone() {
+            if left == right {
+                continue;
+            }
+
+            overlaps_counter += if is_overlaping(left, right) { 1 } else { 0 }
+        }
+
+        if overlaps_counter == 0 {
+            result = Some(left.id);
+        }
+    }
+
+    result
 }
 
 #[test]
 fn probando_day_3_1_2018() {
-    assert_eq!(115424, day_3_1_2018())
+    assert_eq!(Some(117489), day_3_1_2018())
 }
 
 #[test]
 fn probando_day_3_2_2018() {
-    assert_eq!(32, day_3_2_2018())
+    assert_eq!(Some(32), day_3_2_2018())
 }
--- src/year_2018/runner_2018.rs
+++ src/year_2018/runner_2018.rs
@@ -10,6 +10,7 @@ pub fn run_advent_of_code_2018() {
         "* Day 1 - part 2 => {result}",
         result = year_2018::day_1_2018::day_1_2_2018()
     );
+
     println!(
         "* Day 2 - part 1 => {result}",
         result = year_2018::day_2_2018::day_2_1_2018()
@@ -19,5 +20,21 @@ pub fn run_advent_of_code_2018() {
         result = year_2018::day_2_2018::day_2_2_2018()
     );
 
-    year_2018::day_3_2018::day_3_1_2018();
+    println!(
+        "* Day 3 - part 1 => {result:?}",
+        result = year_2018::day_3_2018::day_3_1_2018()
+    );
+    println!(
+        "* Day 3 - part 2 => {result:?}",
+        result = year_2018::day_3_2018::day_3_2_2018()
+    );
+
+    println!(
+        "* Day 4 - part 1 => {result:?}",
+        result = year_2018::day_4_2018::day_4_1_2018()
+    );
+    println!(
+        "* Day 4 - part 2 => {result:?}",
+        result = year_2018::day_4_2018::day_4_2_2018()
+    );
 }
